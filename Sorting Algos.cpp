ccc4708976fcf19201aaace4c562366ca3ab54cc
Albert Sim <83002245+chiftea@users.noreply.github.com>
2022-03-15 18:26:11 +0300

    Update Sorting Algos.cpp

--- Sorting Algos.cpp
+++ Sorting Algos.cpp
@@ -6,13 +6,13 @@
 #include <vector>
 
 
-#include "../radix.h"
-#include "../merge.h"
+#include "/radix.h"
+#include "/merge.h"
 
-#include "../get_max.h"
-#include "../print.h"
-#include "../populate_array.h"
-#include "../read.h"
+#include "/get_max.h"
+#include "/print.h"
+#include "/populate_array.h"
+#include "/read.h"
 #include "Shell.h"
 #include "heap.h"
 #include "quicksort.h"
@@ -28,111 +28,14 @@ using namespace std;
 
 
 
-/*  for algorithm:
+/*  
 		for test:
 			populate array w/ random numbers following test conditions /-/-/-/
 				backup array (without backup-ing the time comparisons between sorting algorithms are irrelevant) /-/-/-/-/
 				sort using own algorithm {measure time}
 					rollback array or use copy
 				sort using default c++ sort {measure time}
-
 */
-
-//int draw()
-//{
-//	int i = 0;
-//	int sel = 1;
-//	int c = 0;
-//
-//	while (i == 0) {
-//		switch (sel)
-//		{
-//		case 1:
-//			system("cls");
-//			cout << ">> 1. RadixSort" << "\n";
-//			cout << "   2. MergeSort" << "\n";
-//			cout << "   3. ShellSort" << "\n";
-//			cout << "   4. Heapsort " << "\n";
-//			cout << "   5. Quicksort" << "\n";
-//			cout << "   6. EXIT     " << "\n";
-//			//system("pause");
-//			break;
-//
-//		case 2:
-//			system("cls");
-//			cout << "   1. RadixSort" << "\n";
-//			cout << ">> 2. MergeSort" << "\n";
-//			cout << "   3. ShellSort" << "\n";
-//			cout << "   4. Heapsort " << "\n";
-//			cout << "   5. Quicksort" << "\n";
-//			cout << "   6. EXIT     " << "\n";
-//			//system("pause");
-//			break;
-//
-//		case 3:
-//			system("cls");
-//			cout << "   1. RadixSort" << "\n";
-//			cout << "   2. MergeSort" << "\n";
-//			cout << ">> 3. ShellSort" << "\n";
-//			cout << "   4. Heapsort " << "\n";
-//			cout << "   5. Quicksort" << "\n";
-//			cout << "   6. EXIT     " << "\n";
-//			//system("pause");
-//			break;
-//		case 4:
-//			system("cls");
-//			cout << "   1. RadixSort" << "\n";
-//			cout << "   2. MergeSort" << "\n";
-//			cout << "   3. ShellSort" << "\n";
-//			cout << ">> 4. Heapsort " << "\n";
-//			cout << "   5. Quicksort" << "\n";
-//			cout << "   6. EXIT     " << "\n";
-//			break;
-//		case 5:
-//			system("cls");
-//			cout << "   1. RadixSort" << "\n";
-//			cout << "   2. MergeSort" << "\n";
-//			cout << "   3. ShellSort" << "\n";
-//			cout << "   4. Heapsort " << "\n";
-//			cout << ">> 5. Quicksort" << "\n";
-//			cout << "   6. EXIT     " << "\n";
-//			break;
-//		case 6:
-//			system("cls");
-//			cout << "   1. RadixSort" << "\n";
-//			cout << "   2. MergeSort" << "\n";
-//			cout << "   3. ShellSort" << "\n";
-//			cout << "   4. Heapsort " << "\n";
-//			cout << "   5. Quicksort" << "\n";
-//			cout << ">> 6. EXIT     " << "\n";
-//		}
-//
-//		//Sleep(100);
-//
-//
-//		int c = _getch();
-//
-//		switch (c) {
-//		case KEY_UP:
-//			sel--;
-//			if (sel <= 0) {
-//				sel = 6;
-//			}
-//			break;
-//
-//		case KEY_DOWN:
-//			sel++;
-//			if (sel > 6) {
-//				sel = 1;
-//			}
-//			break;
-//		case ENTER:
-//			i = 1;
-//			break;
-//		}
-//	}
-//	return sel;
-//}
 
 void print_res(int res, string sort)
 {
